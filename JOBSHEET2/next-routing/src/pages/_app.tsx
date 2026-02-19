79751074d231dac869e1339e089175ba7fd807cf
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-02-19 17:09:02 +0700

    feat: add Navbar component and update global styles; create HomePage with introductory content; add routing images

--- JOBSHEET2/next-routing/src/pages/_app.tsx
+++ JOBSHEET2/next-routing/src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import "@/styles/globals.css";
+import Navbar from "@/components/layouts/navbar";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Navbar />
+      <Component {...pageProps} />
+    </div>
+  );
 }
