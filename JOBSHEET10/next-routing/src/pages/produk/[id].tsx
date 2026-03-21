a5b14d283762dc25e53a7dc7c953cf96ad6f6ff5
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-03-21 21:29:45 +0700

    feat: add SSR implementation and update product detail page with new images

--- JOBSHEET10/next-routing/src/pages/produk/[id].tsx
+++ JOBSHEET10/next-routing/src/pages/produk/[id].tsx
@@ -2,20 +2,20 @@ import { useRouter } from "next/router";
 import fetcher from "@/utils/swr/fetcher";
 import useSWR from "swr";
 import DetailsProduk from "../../views/DetailProduct";
-import { produkType } from "@/types/Product.type";
+import { ProductType } from "@/types/Product.type";
 
 const HalamanProduk = () => {
-  const { query } = useRouter();
-  const { data, isLoading } = useSWR(
-    query.id ? `/api/produk/${query.id}` : null,
-    fetcher,
-  );
+  // const { query } = useRouter();
+  // const { data, isLoading } = useSWR(
+  //   query.id ? `/api/produk/${query.id}` : null,
+  //   fetcher,
+  // );
 
-  const product = (data?.data ?? null) as produkType | null;
+  // const product = (data?.data ?? null) as produkType | null;
 
-  if (isLoading || !product) {
-    return <p style={{ padding: 16 }}>Memuat detail produk...</p>;
-  }
+  // if (isLoading || !product) {
+  //   return <p style={{ padding: 16 }}>Memuat detail produk...</p>;
+  // }
 
   return (
     <div>
@@ -25,3 +25,14 @@ const HalamanProduk = () => {
 };
 
 export default HalamanProduk;
+
+export async function getServerSideProps({ params }: { params: { id: string } }) {
+  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const respone = await res.json();
+  
+  return {
+    props: {
+      product: respone.data,
+    },
+  };
+}
