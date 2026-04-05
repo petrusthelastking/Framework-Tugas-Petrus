76bb917b17f21ed4a153657a71b6f314b62cef85
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-04-05 20:19:22 +0700

    feat(register): enhance registration form with error handling and additional fields

--- JOBSHEET14/next-routing/src/utils/db/servicefirebase.ts
+++ JOBSHEET14/next-routing/src/utils/db/servicefirebase.ts
@@ -62,9 +62,9 @@ export async function signUp(
     }));
     // console.log("Query result:", data);
 
-    if (data.length > 0) {
+    if (data.length === 0) {
         // user belum ada → boleh daftar
-        // await addDoc(collection(db, "users"), userData);
+        await addDoc(collection(db, "users"), userData);
         // console.log("User registered:", data);
         callback({
             status: "success",
