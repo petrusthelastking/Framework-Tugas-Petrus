50f6738d52178baf818eb86957d9407ba1072129
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-04-05 19:52:54 +0700

    feat: add register view with form and styling

--- JOBSHEET14/next-routing/src/views/auth/register/index.tsx
+++ JOBSHEET14/next-routing/src/views/auth/register/index.tsx
@@ -1,19 +1,30 @@
 import Link from "next/link";
-import styles from './register.module.css';
+import style from "./register.module.scss";
 
 const TampilanRegister = () => {
   return (
-    <div className={styles.register}>
-      <div className={styles.card}>
-        <h1 className={styles.title}>Halaman Register</h1>
-        <input className={styles.input} type="text" placeholder="Username" />
-        <input className={styles.input} type="email" placeholder="Email" />
-        <input className={styles.input} type="password" placeholder="Password" />
-        <button className={styles.button}>Daftar</button>
-        <Link href="/auth/login" className={styles.link}>Sudah punya akun? Login</Link>
+    <div className={style.register}>
+      <h1 className={style.register__title}>Halaman Register</h1>
+
+      <div className={style.register__form__item}>
+        <label
+          htmlFor="email"
+          className={style.register__form__item__label}
+        >
+          Email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Email"
+          className={style.register__form__item__input}
+        />
       </div>
+
+      <Link href="/auth/login">Ke Halaman Login</Link>
     </div>
   );
-}
+};
 
 export default TampilanRegister;
