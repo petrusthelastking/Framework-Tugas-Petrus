51259fd946e0a6f549ee962e379c63710253bb03
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-04-05 11:31:37 +0700

    feat: add images for Jobsheet 13 and enhance Navbar styling with authentication buttons

--- JOBSHEET13/next-routing/src/components/layouts/navbar/index.tsx
+++ JOBSHEET13/next-routing/src/components/layouts/navbar/index.tsx
@@ -1,11 +1,20 @@
 import syles from './Navbar.module.css';
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Navbar = () => {
+  const {data} = useSession()
+  //const { data: session } = useSession()
+  // console.log("session", session)
   return (
   <div className={syles.navbar}>
     <div className="big">
       Navbar
     </div>
+    {data ? (
+      <button onClick={() => signOut()}>Sign Out</button>
+    ) : (
+      <button onClick={() => signIn()}>Sign In</button>
+    )}
   </div>
   );
 };
