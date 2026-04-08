84c490ad08b6909028b3b68b146887190b716c84
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-04-08 14:46:11 +0700

    feat(auth): implement Google sign-in functionality and update user data in database

--- JOBSHEET16/next-routing/src/utils/db/servicefirebase.ts
+++ JOBSHEET16/next-routing/src/utils/db/servicefirebase.ts
@@ -9,6 +9,7 @@ import {
     addDoc,
     where,
     deleteDoc,
+    updateDoc,
 } from "firebase/firestore";
 import app from "./firebase";
 import bcrypt from "bcrypt";
@@ -100,5 +101,46 @@ export async function signUp(
                     message: error.message,
                 });
             });
+    }
+}
+
+export async function signInWithGoogle(userData: any, callback: any) {
+    try {
+        const q = query(
+            collection(db, "users"),
+            where("email", "==", userData.email),
+        );
+
+        const querySnapshot = await getDocs(q);
+        const data: any = querySnapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+        }));
+
+        if (data.length > 0) {
+            // User sudah ada, update data
+            userData.role = data[0].role;
+            await updateDoc(doc(db, "users", data[0].id), userData);
+            callback({
+                status: true,
+                message: "User registered and logged in with Google",
+                data: userData,
+            });
+        } else {
+            // User baru, tambah data
+            userData.role = "member";
+            await addDoc(collection(db, "users"), userData);
+            callback({
+                status: true,
+                message: "User registered and logged in with Google",
+                data: userData,
+            });
+        }
+    } catch (error: any) {
+        // Tangani error di sini
+        callback({
+            status: false,
+            message: "Failed to register user with Google",
+        });
     }
 }
