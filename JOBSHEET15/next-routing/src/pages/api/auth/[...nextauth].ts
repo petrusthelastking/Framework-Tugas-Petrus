78cdcab9cbeee90f0e193c1fd2142cac6ef2709b
Petrus Tyang A.r <petruskmkz@gmail.com>
2026-04-08 11:44:13 +0700

    feat(auth): enhance authentication flow with role management and password validation

--- JOBSHEET15/next-routing/src/pages/api/auth/[...nextauth].ts
+++ JOBSHEET15/next-routing/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { signIn } from "@/utils/db/servicefirebase";
 import NextAuth, { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   session: {
@@ -10,24 +12,31 @@ export const authOptions: NextAuthOptions = {
     CredentialsProvider({
       name: "credentials",
       credentials: {
-        fullname: { label: "Full Name", type: "text" },
+        // fullname: { label: "Full Name", type: "text" },
         email: { label: "Email", type: "email" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        const user: any = {
-          id: "1",
-          email: credentials?.email,
-          password: credentials?.password,
-          fullname: credentials?.fullname,
-        }
+        if (!credentials?.email || !credentials?.password) return null;
+
+        const user: any = await signIn(credentials.email);
 
         if (user) {
-          // console.log("user", user)
-          return user
-        } else {
-          return null
+          const isPasswordValid = await bcrypt.compare(
+            credentials.password,
+            user.password,
+          );
+          if (isPasswordValid) {
+            // Pastikan mengembalikan object user yang bersih
+            return {
+              id: user.id,
+              email: user.email,
+              fullname: user.fullname,
+              role: user.role,
+            };
+          }
         }
+        return null;
       },
     }),
   ],
@@ -36,6 +45,7 @@ export const authOptions: NextAuthOptions = {
       if (account?.provider === "credentials" && user) {
         token.email = user.email
         token.fullname = user.fullname
+        token.role = user.role;
       }
       //   console.log("jwt callback", { token, account, profile, user })
       return token
@@ -46,6 +56,9 @@ export const authOptions: NextAuthOptions = {
       }
       if (token.fullname) {
         session.user.fullname = token.fullname
+      }
+      if (token.role) {
+        session.user.role = token.role;
       }
       //   console.log("session callback", { session, token })
       return session
